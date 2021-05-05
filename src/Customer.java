a9ea32584a41112ce8256ce3833692c81671f94f
Jonathan <57756132+jdk-21@users.noreply.github.com>
2021-05-05 10:10:12 +0200

    refactor move method getCharge

--- src/Customer.java
+++ src/Customer.java
@@ -44,22 +44,8 @@ class Customer {
         return result.toString();
     }
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR -> {
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-            }
-            case Movie.NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
-            case Movie.CHILDREN -> {
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-            }
-        }
-        return thisAmount;
+    private double amountFor(Rental aRental) {
+        return aRental.getCharge();
     }
 
 }
